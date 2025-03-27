38df9639b22403ac6b6ee69d3286a640b566fdd3
Pithiya-Hardik <pithiyahardik18@gmail.com>
2025-03-27 12:17:37 +0530

    add accounting fileds

--- plant_maintenance/plant_maintenance/doctype/plant/plant.js
+++ plant_maintenance/plant_maintenance/doctype/plant/plant.js
@@ -1,9 +1,33 @@
 // Copyright (c) 2024, Rahul Sarkar and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Plant", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Plant", {
+	onload:function(frm) {
+        let account_type = ["Tax", "Chargeable", "Income Account", "Expenses Included In Valuation", "Expenses Included In Asset Valuation"]
+        frm.set_query("machine_cost_account", function(){
+            return{
+                filters:{
+                    company:frm.doc.company,
+                    account_type:  ["in", account_type]
+                }
+            }
+        })
+        frm.set_query("labour_cost_account", function(){
+            return{
+                filters:{
+                    company:frm.doc.company,
+                    account_type: ["in", account_type]
+                }
+            }
+        })
+        frm.set_query("over_heads_cost_account", function(){
+            return{
+                filters:{
+                    company:frm.doc.company,
+                    account_type: ["in", account_type]
+                }
+            }
+        })
+	},
+});
 //test
